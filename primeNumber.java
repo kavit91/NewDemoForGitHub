90af7f14f15909777e29726663e881657d848b07
kavit91 <75495535+kavit91@users.noreply.github.com>
2021-05-19 22:28:14 +0530

    Create primeNumber.java

---
+++ primeNumber.java
@@ -0,0 +1,15 @@
+public class PrimeNumber{
+  
+ public static viod main(String[] args){
+  for(int i=1;i<=100;i++)
+  {
+    int tem=0;
+    for(int j=2;j<=i-1;j++){
+     if(i%k==0)
+       tem++;
+    }
+    if(tem==0 && i!=1)
+      System.out.println(i);
+  }
+ }
+}
